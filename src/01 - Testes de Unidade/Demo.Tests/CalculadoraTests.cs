e150e0c7ee2e557c116f0653840f8490e2e7de12
jorgelucas <jorgelucasl91@gmail.com>
2021-06-11 17:16:26 -0300

    [FEATURE] Criação da solution, projeto demo e projeto de testes

---
+++ src/01 - Testes de Unidade/Demo.Tests/CalculadoraTests.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+
+namespace Demo.Tests
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void Calculadora_Somar_RetornarValorSoma()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            // Act
+            var resultado = calculadora.Somar(5, 5);
+
+            // Assert
+            Assert.Equal(10, resultado);
+        }
+
+        [Theory]
+        [InlineData(2, 2, 4)]
+        [InlineData(4, 4, 8)]
+        [InlineData(5, 5, 10)]
+        [InlineData(15, 5, 20)]
+        [InlineData(9, 9, 18)]
+        public void Calculadora_Somar_RetornarValoresCorretos(double valo1, double valor2, double total)
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            // Act
+            var resultado = calculadora.Somar(valo1, valor2);
+
+            // Assert
+            Assert.Equal(total, resultado);
+        }
+
+    }
+}
---
+++ src/01 - Testes de Unidade/Demo/Calculadora.cs
@@ -0,0 +1,15 @@
+﻿namespace Demo
+{
+    public class Calculadora
+    {
+        public double Somar(double valo1, double valor2)
+        {
+            return valo1 + valor2;
+        }
+
+        public int Dividir(int valo1, int valor2)
+        {
+            return valo1 / valor2;
+        }
+    }
+}
