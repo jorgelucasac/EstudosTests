d9e9751e5f5ccb432beef40b14ecf1fe2f7bade3
jorgelucas <jorgelucasl91@gmail.com>
2021-06-14 15:28:27 -0300

    [FEATURE] Utilizando Traits
    - utilizando traits para organizar melhor os testes

---
+++ src/01 - Testes de Unidade/Features.Tests/01 - Traits/ClienteTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Features.Clientes;
+using Xunit;
+
+namespace Features.Tests
+{
+    public class ClienteTests
+    {
+        [Fact(DisplayName = "Novo Cliente Válido")]
+        [Trait("Categoria", "Cliente Trait Testes")]
+        public void Cliente_NovoCliente_DeveEstarValido()
+        {
+            // Arange 
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "Jorge Lucas",
+                "Alves",
+                new DateTime(1995, 08, 28),
+                "jorge@email.com",
+                true,
+                DateTime.Now
+            );
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert
+            Assert.True(result);
+            Assert.Equal(0, cliente.ValidationResult.Errors.Count);
+
+        }
+
+        [Fact(DisplayName = "Novo Cliente Inválido")]
+        [Trait("Categoria", "Cliente Trait Testes")]
+        public void Cliente_NovoCliente_DeveEstarInvalido()
+        {
+            // Arange 
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now, 
+                "jorge1email.com",
+                true,
+                DateTime.Now
+            );
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert
+            Assert.False(result);
+            Assert.NotEqual(0, cliente.ValidationResult.Errors.Count);
+
+        }
+    }
+}
