fc2cc467e4248e24961bf234aea3c8258ff30b69
jorgelucas <jorgelucasl91@gmail.com>
2021-06-14 16:51:30 -0300

    [FEATURE] Simulando retorno de métodos com Mock

--- src/01 - Testes de Unidade/Features.Tests/05 - Mock/ClienteServiceTests.cs
+++ src/01 - Testes de Unidade/Features.Tests/05 - Mock/ClienteServiceTests.cs
@@ -10,7 +10,7 @@ namespace Features.Tests
     [Collection(nameof(ClienteBogusCollection))]
     public class ClienteServiceTests
     {
-        readonly ClienteTestsBogusFixture _clienteTestsBogus;
+        private readonly ClienteTestsBogusFixture _clienteTestsBogus;
 
         public ClienteServiceTests(ClienteTestsBogusFixture clienteTestsFixture)
         {
@@ -43,14 +43,45 @@ namespace Features.Tests
         [Trait("Categoria", "Cliente Service Mock Tests")]
         public void ClienteService_Adicionar_DeveFalharDevidoClienteInvalido()
         {
+            // Arrange
+            var cliente = _clienteTestsBogus.GerarClienteInvalido();
+            var clienteRepository = new Mock<IClienteRepository>();
+            var mediatr = new Mock<IMediator>();
 
+            var clienteService = new ClienteService(clienteRepository.Object, mediatr.Object);
+
+            // Act
+            clienteService.Adicionar(cliente);
+
+            // Asert
+            Assert.False(cliente.EhValido());
+
+            //validando se os metodos foram chamados na ação "clienteService.Adicionar"
+            clienteRepository.Verify(r => r.Adicionar(cliente), Times.Never);
+            mediatr.Verify(m => m.Publish(It.IsAny<INotification>(), CancellationToken.None), Times.Never);
         }
 
         [Fact(DisplayName = "Obter Clientes Ativos")]
         [Trait("Categoria", "Cliente Service Mock Tests")]
         public void ClienteService_ObterTodosAtivos_DeveRetornarApenasClientesAtivos()
         {
+            // Arrange
+            var clienteRepository = new Mock<IClienteRepository>();
+            var mediatr = new Mock<IMediator>();
 
+            // Setando o retorno do método
+            clienteRepository.Setup(c => c.ObterTodos())
+                .Returns(_clienteTestsBogus.ObterClientesVariados());
+
+            var clienteService = new ClienteService(clienteRepository.Object, mediatr.Object);
+
+            // Act
+            var clientes = clienteService.ObterTodosAtivos().ToList();
+
+            // Assert
+            clienteRepository.Verify(r => r.ObterTodos(), Times.Once);
+            Assert.True(clientes.Any());
+            Assert.False(clientes.Count(c=> !c.Ativo)> 0);
         }
     }
 }
