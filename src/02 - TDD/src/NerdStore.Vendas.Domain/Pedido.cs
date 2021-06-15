f65b2ba432cdb67c89143579e8242cac112dc5c3
jorgelucas <jorgelucasl91@gmail.com>
2021-06-15 18:04:23 -0300

    [FEATURE] Testes iniciais de adicionar pedido
    - criado o teste que valida se o valor foi atualizado quando adicionar um novo item

---
+++ src/02 - TDD/src/NerdStore.Vendas.Domain/Pedido.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class Pedido
+    {
+        public decimal ValorTotal { get; private set; }
+        private readonly List<PedidoItem> _pedidoItens;
+        public IReadOnlyCollection<PedidoItem> PedidoItens => _pedidoItens.AsReadOnly();
+
+        public Pedido()
+        {
+            _pedidoItens = new List<PedidoItem>();
+        }
+
+        public void AdicionarItem(PedidoItem pedidoItem)
+        {
+            _pedidoItens.Add(pedidoItem);
+            ValorTotal = _pedidoItens.Sum(i=> i.CalcularValorTotal());
+        }
+    }
+}
---
+++ src/02 - TDD/src/NerdStore.Vendas.Domain/PedidoItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class PedidoItem
+    {
+        public Guid ProdutoId { get; private set; }
+        public string ProdutoNome { get; private set; }
+        public int Quantidade { get; private set; }
+        public decimal ValorUnitario { get; private set; }
+
+        public PedidoItem(Guid produtoId, string produtoNome, int quantidade, decimal valorUnitario)
+        {
+            ProdutoId = produtoId;
+            this.ProdutoNome = produtoNome;
+            this.Quantidade = quantidade;
+            this.ValorUnitario = valorUnitario;
+        }
+
+        internal decimal CalcularValorTotal()
+        {
+            return Quantidade * ValorUnitario;
+        }
+    }
+}
