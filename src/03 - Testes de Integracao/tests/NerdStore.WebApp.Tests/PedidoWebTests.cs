e755b59e5ee08edf88b78243483f49922cf84154
jorgelucas <jorgelucasl91@gmail.com>
2021-06-22 17:26:30 -0300

    [FEATURE] Pedidos - Web - pt2
    - finalização do teste de adicionar item ao carrinho

--- src/03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/PedidoWebTests.cs
+++ src/03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/PedidoWebTests.cs
@@ -10,6 +10,7 @@ using Xunit;
 
 namespace NerdStore.WebApp.Tests
 {
+    [Collection(nameof(IntegrationWebTestsFixtureCollection))]
     public class PedidoWebTests
     {
         private readonly IntegrationTestsFixture<StartupWebTest> _testsFixture;
@@ -28,15 +29,21 @@ namespace NerdStore.WebApp.Tests
             var produtoId = new Guid("f53b3f5a-9304-475d-b26a-c65e7f510fa6");
             const int quantidade = 2;
 
+            await _testsFixture.RealizarLoginWeb();
+
             var initialResponse = await _testsFixture.Client.GetAsync($"/produto-detalhe/{produtoId}");
             initialResponse.EnsureSuccessStatusCode();
 
+
+            var antiForgeryToken = _testsFixture.ObterAntiForgeryToken(await initialResponse.Content.ReadAsStringAsync());
+
             var formData = new Dictionary<string, string>
             {
+                {_testsFixture.AntiForgeryFieldName, antiForgeryToken},
                 {"Id", produtoId.ToString()},
                 {"quantidade", quantidade.ToString()}
             };
-
+           
             var postRequest = new HttpRequestMessage(HttpMethod.Post, "/meu-carrinho")
             {
                 Content = new FormUrlEncodedContent(formData)
@@ -56,8 +63,9 @@ namespace NerdStore.WebApp.Tests
 
 
             var formQuantidade = html?.FirstOrDefault(c => c.Id == "quantidade")?.GetAttribute("value")?.ApenasNumeros();
-            var valorUnitario = html?.FirstOrDefault(c => c.Id == "valorUnitario")?.TextContent.Split(".")[0]?.ApenasNumeros();
-            var valorTotal = html?.FirstOrDefault(c => c.Id == "valorTotal")?.TextContent.Split(".")[0]?.ApenasNumeros();
+            var valorUnitario = html?.FirstOrDefault(c => c.Id == "valorUnitario")?.TextContent.Split(",")[0]?.ApenasNumeros();
+            var valorTotal = html?.FirstOrDefault(c => c.Id == "valorTotal")?.TextContent.Split(",")[0]?.ApenasNumeros();
+            Assert.Equal(valorTotal, valorUnitario * formQuantidade);
         }
     }
 }
