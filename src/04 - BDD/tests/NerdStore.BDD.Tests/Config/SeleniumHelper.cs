2a0751bf3cc1a3aa090f3b53750dd8dab38377a7
jorgelucas <jorgelucasl91@gmail.com>
2021-07-01 19:34:57 -0300

    [FEATURE] Adicionando pacoste do Selenium

---
+++ src/04 - BDD/tests/NerdStore.BDD.Tests/Config/SeleniumHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace NerdStore.BDD.Tests.Config
+{
+    public class SeleniumHelper
+    {
+    }
+
+    public class ConfigurationHelper
+    {
+    }
+
+    public enum Browser
+    {
+        Chrome,
+        Firefox
+    }
+
+    public static class WebDriverFactory
+    {
+    }
+}
